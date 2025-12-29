a0ec9e2eaf8ffd84974f93f4501802416bb1c7a0
Guy First <guyfi2@mta.ac.il>
2025-12-29 14:31:28 +0200

    added toggle watchlist functionallity

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import MoviesGrid from './components/MoviesGrid';
 import Watchlist from './components/Watchlist';
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+  
   return (
     <div className="App">
       <div className='container'>
         <Header> </Header>
-
         <Router>
           <nav>
             <ul>
               <il>
-                <Link to="/">Home    </Link>
+                <Link to="/">Home</Link>
               </il>
               <il>
                 <Link to="/WatchList">Watclist</Link>
@@ -24,9 +33,8 @@ function App() {
             </ul>
           </nav>
           <Routes>
-            <Route path="/" element={<MoviesGrid/>}></Route>
+            <Route path="/" element={<MoviesGrid movies={movies}/>}></Route>
             <Route path="/Watchlist" element={<Watchlist/>}></Route>
-
           </Routes>
         </Router>
       </div>
